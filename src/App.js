bfd85c843f9501242cdcfe53310b8f05f6b6be44
May <k.bureechai@gmail.com>
2024-11-27 06:47:27 +0700

    added rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ export default function App() {
     <div className="App">
       Weather App
       <footer>
-        This project was coded by {" "}
+        This project was coded by{" "}
         <a
           href="https://www.linkedin.com/in/kwanta-bureechai-543821256/"
           target="_blank"
+          rel="noreferrer"
         >
           Kwanta
-        </a>
-        {" "}and is{" "}
+        </a>{" "}
+        and is{" "}
         <a
           href="https://github.com/K-Bureechai/react-weather-app"
           target="_blank"
+          rel="noreferrer"
         >
           open source on GitHub
         </a>
