5ba7f2e2307c2668f4f29bf8ccd8a9a47ec8df38
May <k.bureechai@gmail.com>
2024-11-27 06:44:28 +0700

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        This project was coded by {" "}
+        <a
+          href="https://www.linkedin.com/in/kwanta-bureechai-543821256/"
+          target="_blank"
+        >
+          Kwanta
+        </a>
+        {" "}and is{" "}
+        <a
+          href="https://github.com/K-Bureechai/react-weather-app"
+          target="_blank"
+        >
+          open source on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
-
