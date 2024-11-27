ff89ab6be0d0485de1d7c32da0a15bd11345afdb
May <k.bureechai@gmail.com>
2024-11-27 07:15:11 +0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/kwanta-bureechai-543821256/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Kwanta
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/K-Bureechai/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open source on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/kwanta-bureechai-543821256/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Kwanta
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/K-Bureechai/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open source on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
