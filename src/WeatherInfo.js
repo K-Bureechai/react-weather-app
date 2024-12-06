c09cf728053e6c974b65dad8d8ad2c18afe31fda
May <k.bureechai@gmail.com>
2024-12-06 21:42:38 +0700

    created WeatherTemperature component

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -8,20 +9,16 @@ export default function WeatherInfo(props) {
       <h1>{props.data.city}</h1>
       <div className="row">
         <div className="col-6">
-          <WeatherIcon code={props.data.icon} />
-          <span className="temperature">
-            {Math.round(props.data.temperature)}
-          </span>
-          <div className="unit">
-            <span className="celsius"> °C </span>
-            <span className="fahrenheit"> °F</span>
-          </div>
-          <div className="detail mt-1">
-            <ul className="ps-4">
-              <li>Precipitation: 0%</li>
-              <li>Humidity: {props.data.humidity}%</li>
-              <li>Wind: {props.data.wind} km/h</li>
-            </ul>
+          <div className="left">
+            <WeatherIcon code={props.data.icon} />
+            <WeatherTemperature celsius={props.data.temperature} />
+            <div className="detail mt-1">
+              <ul className="ps-4">
+                <li>Precipitation: 0%</li>
+                <li>Humidity: {props.data.humidity}%</li>
+                <li>Wind: {props.data.wind} km/h</li>
+              </ul>
+            </div>
           </div>
         </div>
         <div className="col-6">
