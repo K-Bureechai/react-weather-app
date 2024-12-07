516a17d1877be260161d7f932e48602f5d7a6af7
May <k.bureechai@gmail.com>
2024-12-07 17:58:39 +0700

    added forecast API

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -3,23 +3,24 @@ import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 import axios from "axios";
 
-export default function WeatherForecast() {
-    function handleResponse(response) {
-        console.log(response.data);
-    }
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+console.log(props);
 
-    let apiKey = "85bbd3d16a2dfe0ecf253c7ae1e8fe03";
-    let longitude = 100;
-    let latitude = 13;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
-  
-  axios.get(apiUrl).then(handleResponse);
-    return (
+  let apiKey = "a1b23b3d4098a0a16ftb2b02cf94o4d8";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
+
+  https: axios.get(apiUrl).then(handleResponse);
+  return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
           <div className="WeatherForecast-day">Thu</div>
-          <WeatherIcon code="01d" size={36}/>
+          <WeatherIcon code="01d" size={36} />
           <div className="WeatherForecast-temperatures">
             <span className="WeatherForecast-temperature-max">19°</span>
             <span className="WeatherForecast-temperature-min">10°</span>
